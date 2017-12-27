691c642dc38ee9d48d42036b820724295c015ce1
Arstan <arstanrys@gmail.com>
2017-12-28 02:03:40 +0600

    add parallelism and acceleration printing on each delta time

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 typedef struct _body 
 {
-    int id;
+    float id;
     float x, y; //position
     float ax, ay; //acceleration
     float vx, vy; //velocity
@@ -34,7 +34,7 @@ int main(int argc, char **argv) {
     printf("%f/n", DELTA_TIME);
     for(int i = 0; i < NUMBER_OF_BODIES; ++i)
     {
-        printf("Body #%d\n", bodies[i].id);
+        printf("Body #%f\n", bodies[i].id);
 		printf("x = %f, y = %f\n", bodies[i].x, bodies[i].y);
 		printf("ax = %f, ay = %f\n", bodies[i].ax, bodies[i].ay);
 		printf("vx = %f, vy = %f\n", bodies[i].vx, bodies[i].vy);
@@ -43,28 +43,46 @@ int main(int argc, char **argv) {
 
     MPI_Init(&argc, &argv);
 
+	MPI_Datatype MPI_BODY;
+	MPI_Type_contiguous(5, MPI_FLOAT, &MPI_BODY);
+	MPI_Type_commit(&MPI_BODY);
+
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
-    for(size_t i = 0 ; i < NUMBER_OF_BODIES; ++i ) 
-    {
-        float total_ax = 0.0f, total_ay = 0.0f;
-        for(size_t j = 0 ; j < NUMBER_OF_BODIES; ++j ) 
+
+    int items_per_process = NUMBER_OF_BODIES / world_size; 
+    for (float time_active = 0; time_active < DURATION; time_active += DELTA_TIME) {
+        body *local_bodies =  (body *) malloc(sizeof(*local_bodies) * items_per_process);
+
+        MPI_Scatter(bodies, items_per_process, MPI_BODY, local_bodies, items_per_process, MPI_BODY, 0, MPI_COMM_WORLD);
+
+        for(size_t i = 0 ; i < items_per_process; ++i ) 
         {
-            if( i == j ) { continue ; }
-            float ax, ay;
-            CalculateNewtonGravityAcceleration(&bodies[i], &bodies[j], &ax , &ay);
-            total_ax += ax;
-            total_ay += ay;
+            float total_ax = 0.0f, total_ay = 0.0f;
+            for(size_t j = 0 ; j < NUMBER_OF_BODIES; ++j ) 
+            {
+                if( local_bodies[i].id == bodies[j].id ) { continue ; }
+                float ax, ay;
+                CalculateNewtonGravityAcceleration(&local_bodies[i], &local_bodies[j], &ax , &ay);
+                total_ax += ax;
+                total_ay += ay;
+            }
+            local_bodies[i].ax = total_ax;
+            local_bodies[i].ay = total_ay;
         }
-        bodies[i].ax = total_ax;
-        bodies[i].ay = total_ay;
+
+        MPI_Gather(&local_bodies, 1, MPI_BODY, bodies, items_per_process, MPI_BODY, 0, MPI_COMM_WORLD);
+
+        MPI_Barrier(MPI_COMM_WORLD);
+	    free(local_bodies);
+
+        if(rank == 0)
+        {
+            integratebodies(bodies);
+        }
     }
-
-    integratebodies(bodies);
-
     MPI_Finalize();
 
     return 0;
@@ -77,6 +95,7 @@ static void integratebodies (body *bodies)
 		bodies[i].vy += bodies[i].ay * DELTA_TIME ;
 		bodies[i].x += bodies[i].vx * DELTA_TIME ;
 		bodies[i].y += bodies[i].vy * DELTA_TIME ;
+        printf("%f  %f\n", bodies[i].ax, bodies[i].ay);
 	}
 }
 
